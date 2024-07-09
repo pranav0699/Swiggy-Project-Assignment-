36ced5271057655bf3cfb9a48cb4793a3d67c48b
D5-80786-Pranav-Adagonda-Patil <pranavpatil060899@gmail.com>
2024-07-09 14:52:05 +0530

    GameEngineTest test class added

---
+++ magical-arena/src/Test/GameEngineTest.java
@@ -0,0 +1,27 @@
+package Test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.arena.model.Player;
+import com.arena.service.GameEngine;
+import com.arena.service.RandomDiceRoller;
+import com.arena.service.SimpleArena;
+
+public class GameEngineTest {
+
+	@Test
+	public void testStartGame() {
+		Player playerA = new Player("Player A", 50, 5, 10);
+		Player playerB = new Player("Player B", 100, 10, 5);
+		RandomDiceRoller diceRoller = new RandomDiceRoller();
+		SimpleArena arena = new SimpleArena(diceRoller);
+		GameEngine gameEngine = new GameEngine(arena);
+
+		gameEngine.startGame(playerA, playerB);
+
+		assertTrue(playerA.getHealth() <= 0 || playerB.getHealth() <= 0);
+	}
+
+}
