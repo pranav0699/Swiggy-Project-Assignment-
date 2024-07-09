3a408cd13b341fa40f22f6eba8944d777a4314b7
D5-80786-Pranav-Adagonda-Patil <pranavpatil060899@gmail.com>
2024-07-09 19:43:39 +0530

    all project is done

--- magical-arena/src/com/arena/App.java
+++ magical-arena/src/com/arena/App.java
@@ -8,6 +8,8 @@ import com.arena.service.SimpleArena;
 public class App {
 
 	public static void main(String[] args) {
+		
+		// Here pass the value of player is hardcoded for direct test but it can give from user input also by using Scanner
 		Player playerA = new Player("Player A", 50, 5, 10);
         Player playerB = new Player("Player B", 100, 10, 5);
 
